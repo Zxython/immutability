82064fb1f7acc645954dbe29358c7bdeac5554ca
Zxython <98050815+Zxython@users.noreply.github.com>
2022-06-22 12:35:30 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='immutability',
+      version='0.1',
+      description='Adds immutability class',
+      url='https://github.com/Zxython/immutability/',
+      author='Jerry Gillian',
+      author_email='jerrygillian822@gmail.com',
+      license='Free to use',
+      packages=['immutability'],
+      zip_safe=False)
